6d62179dd75559354e4c650c0b3ca6b39f988e78
tsubasa namatame <nyshk97@gmail.com>
2020-06-18 06:05:35 +0000

    cap rbenv

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,10 @@ set :application, "sample_ror0618"
 set :repo_url, "git@github.com:nyshk97/ror0618.git"
 set :deploy_to, "/var/www/html/namatsuba"
 
+set :rbenv_type, :system
+set :rbenv_ruby, '2.7.1'
+set :rbenv_path, '/usr/local/rbenv'
+
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
